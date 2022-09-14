683a9086e89317696d74c626de7cb7854f3e9a88
bachcoderr <naronbpic@gmail.com>
2022-09-14 21:38:42 +0700

    bachdasua

--- cart.php
+++ cart.php
@@ -48,7 +48,7 @@ session_start();
                     </div>
                 </td>
                 <td id='disPr' class=' align-middle'>199.000đ</td>
-                <td class='align-middle'><button onclick='onAction()' class='choose btn btn-sm btn-primary'><i class='fa fa-times'></i></button></td></tr>";
+                <td class='align-middle'><button onmousedown='onAction()' class='choose btn btn-sm btn-primary'><i class='fa fa-times'></i></button></td></tr>";
             }
         }
      }//function
@@ -279,7 +279,7 @@ session_start();
                     <div class="card-body">
                         <div class="d-flex justify-content-between mb-3 pt-1">
                             <h6 class="font-weight-medium">Giá</h6>
-                            <h6 class="font-weight-medium"><?php sumprice();?>đ</h6>
+                            <h6 class="pri font-weight-medium">0đ</h6>
                         </div>
                         <div class="d-flex justify-content-between">
                             <h6 class="font-weight-medium">Phí Vận Chuyển</h6>
@@ -289,7 +289,7 @@ session_start();
                     <div class="card-footer border-secondary bg-transparent">
                         <div class="d-flex justify-content-between mt-2">
                             <h5 class="font-weight-bold">Tổng tiền</h5>
-                            <h5 class="font-weight-bold"><?php sumprice();?> đ</h5>
+                            <h5 class="pri font-weight-bold">0đ</h5>
                         </div>
                         <button class="btn btn-block btn-primary my-3 py-3">Kiểm tra</button>
                     </div>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+var check = document.getElementsByClassName('choose');
+var changepr = document.getElementsByClassName("pri")
+changepr[0].innerHTML=(check.length) * 199000
+changepr[1].innerHTML=(check.length) * 199000
 function onAction() {
     getElement()
+    
+    setPrice();
 }
 
 function getElement(){
@@ -11,5 +17,33 @@ function getElement(){
             var item = event.target
             item.parentElement.parentElement.remove()
         })
+    }
+    document.getElementsByClassName("pri")[0]=(btn.length-1) * 199000
+    document.getElementsByClassName("pri")[1]=(btn.length-1) * 199000
+}
+function setPrice(){
+    var getItem = document.getElementsByClassName('choose');
+    var price = (getItem.length-1) * 199000
+    var changep = document.getElementsByClassName("pri")
+    //console.log(changep[0].innerHTML)
+    changep[0].innerHTML = price+""
+    changep[1].innerHTML = price+""
+}
+
+function search_cart(){
+    var arrrayPro = document.getElementsByClassName("text-truncate")
+    var getKey = document.getElementById("wv").value;
+    //console.log(arrrayPro[1].innerHTML)
+    //text-truncate
+    var width = arrrayPro.length
+    string1 = getKey.toUpperCase()
+    for(var i = 0; i < width; i++){
+        string2 = arrrayPro[i].innerHTML.toUpperCase()
+        if(string2.includes(string1)){
+            arrrayPro[i].parentElement.parentElement.parentElement.style.display = 'block'
+        }
+        else{
+            arrrayPro[i].parentElement.parentElement.parentElement.style.display = 'none'
+        }
     }
 }
